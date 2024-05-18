116a463605acd28c4349eed73102a95f3e7d1a0e
Mausmato <Singeripad2@gmail.com>
2024-05-18 04:16:41 -0400

    feat(frontend):Get started

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,15 @@
-import './Home.css'
-import { Heading, Button, Flex, Text } from '@radix-ui/themes';
+import React from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook
+import { Heading, Button, Flex, Text } from "@radix-ui/themes";
+import "./Home.css";
+
 const Home = () => {
+  const navigate = useNavigate(); // Initialize the navigate function
+
+  const handleGetStartedClick = () => {
+    navigate("/analyze"); // Navigate to the "Analyze" route
+  };
+
   return (
     <Flex
       gap="4"
@@ -9,11 +18,10 @@ const Home = () => {
       justify="center"
       className="home"
     >
-      <Heading className='title'>
-        Moodify
-      </Heading>
+      <Heading className="title">Moodify</Heading>
       <Text>Music IS emotion.</Text>
-      <Button>Get Started</Button>
+      <Button onClick={handleGetStartedClick}>Get Started</Button>{" "}
+      {/* Attach the click handler */}
     </Flex>
   );
 };
