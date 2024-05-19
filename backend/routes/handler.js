75337aece82433d250e755254834c374fce0aabf
JLi2007 <LiJames9532@gmail.com>
2024-05-19 08:22:19 -0400

    feat(backend): Split the req.body

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/mood', (req,res) => {
-    console.log(req.body);
-})
+    const { p } = req.body;
+    if (typeof p === 'string') {
+        const parts = p.split(' ');
+        const emotion = parts[1];
+        console.log(emotion);
+}});
 
 module.exports = router;
