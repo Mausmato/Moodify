821716cdb75df47416e0e155f95182d437c1727a
JLi2007 <LiJames9532@gmail.com>
2024-05-19 04:22:46 -0400

    feat(frontend): working face detection!

--- frontend/src/pages/Analyze/Analyze.jsx
+++ frontend/src/pages/Analyze/Analyze.jsx
@@ -3,14 +3,14 @@ import { Button } from "@radix-ui/themes";
 import "./Analyze.css";
 import { MagicWandIcon } from "@radix-ui/react-icons";
 
-import sendPrediction from "../../../detection/src/Common/tensorflowPredictions"
+import sendPrediction, {prediction}  from "../../../detection/src/Common/tensorflowPredictions";
 
 const Analyze = () => {
   return (
     <div id="analyze-container" style={{overflow:'none'}}>
       <ManageVideoOnCanvas/>
       <Button 
-        onClick={() => sendPrediction('happy')}
+        onClick={() => sendPrediction(prediction)}
         size="4"
         id="spotify-button"
       >
